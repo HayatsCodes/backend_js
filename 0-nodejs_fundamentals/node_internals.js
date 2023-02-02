c59345afb9faf4fc0f9334c1d0cb82807e7f9d5c
HayatsCodes <hayatscodes@gmail.com>
2023-02-02 12:05:01 +0100

    Fixing a bug

--- 0-nodejs_fundamentals/node_internals.js
+++ 0-nodejs_fundamentals/node_internals.js
@@ -2,6 +2,7 @@
 // It executes basic javascript codes
 
 // Node.js APIs:
-// These are built-in node-js functions that are not part of javascript
+// These are built-in node.js functions that are not part of javascript
 
-// 
+//  Node.js Bindings:
+// This passes node.js APIs to 

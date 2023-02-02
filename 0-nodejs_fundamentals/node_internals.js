4064d8b8254e53f88d3fa1fdb99bc2f90295bdd3
HayatsCodes <hayatscodes@gmail.com>
2023-02-02 21:20:00 +0100

    Adding a new feature

--- 0-nodejs_fundamentals/node_internals.js
+++ 0-nodejs_fundamentals/node_internals.js
@@ -59,3 +59,5 @@
 // This is the last event loop phase that handles closing of files or network connections
 
 // Node.js is best at I/O heavy applications and is built for the web
+
+// The Observer Pattern

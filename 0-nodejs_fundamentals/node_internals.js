6b840e634e0447f3397ad3060cb44f73cf9df9a2
HayatsCodes <hayatscodes@gmail.com>
2023-02-02 12:10:03 +0100

    Updating the documentation

--- 0-nodejs_fundamentals/node_internals.js
+++ 0-nodejs_fundamentals/node_internals.js
@@ -10,4 +10,4 @@
 // libuv:
 // The implementations of the node.js API's can be found here
 
-// Asynchronous I/O: It's a procees by which the v8 engine passes 
+// Asynchronous I/O: It's a procees by which the v8 engine passes node.js APIs to libuv while it continues to run other codes in the background

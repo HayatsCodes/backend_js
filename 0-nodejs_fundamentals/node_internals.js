ceb11fa505e237faa530e99f06aa84ad287d27cd
HayatsCodes <hayatscodes@gmail.com>
2023-02-02 15:30:27 +0100

    Improving performance

--- 0-nodejs_fundamentals/node_internals.js
+++ 0-nodejs_fundamentals/node_internals.js
@@ -37,4 +37,4 @@
 // It provides a minimum of 4 threads and a maximum of 8 threads with all of them waiting for file system asynchronous I/O codes.
 
 // Event Phase Loop:
-// 
+// This are the different phases of an event loop

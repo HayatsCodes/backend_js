f7431e0855e245403c10bc59c11afea8087ddcd6
HayatsCodes <hayatscodes@gmail.com>
2023-02-03 21:35:59 +0100

    Updating the documentation

--- 0-nodejs_fundamentals/node-module_system.js
+++ 0-nodejs_fundamentals/node-module_system.js
@@ -4,4 +4,7 @@ http.request('www.google.com', (res) => {
     res.on('data', (chunk) => {
         console.log(`Data chunk: ${chunk}`);
     });
+    res.on('end', () => {
+        console.log('No more data');
+    });
 });

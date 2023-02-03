993494b400f6290bec72f1d8153b006a37358fa8
HayatsCodes <hayatscodes@gmail.com>
2023-02-03 22:02:59 +0100

    Fixing a bug

--- 0-nodejs_fundamentals/node-module_system.js
+++ 0-nodejs_fundamentals/node-module_system.js
@@ -1,6 +1,19 @@
-const http = require('http');
+// const http = require('http');
 
-const req = http.request('http://www.google.com', (res) => {
+// const req = http.request('http://www.google.com', (res) => {
+//     res.on('data', (chunk) => {
+//         console.log(`Data chunk: ${chunk}`);
+//     });
+//     res.on('end', () => {
+//         console.log('No more data');
+//     });
+// });
+
+// req.end();
+
+const {get} = require('https');
+
+const req = request('https://www.google.com', (res) => {
     res.on('data', (chunk) => {
         console.log(`Data chunk: ${chunk}`);
     });
@@ -8,6 +21,3 @@ const req = http.request('http://www.google.com', (res) => {
         console.log('No more data');
     });
 });
-
-req.end();
-

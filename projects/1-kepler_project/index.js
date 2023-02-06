cf61fc1c585eab5ddfc6141f4d7b00ff020589e7
HayatsCodes <hayatscodes@gmail.com>
2023-02-06 16:53:01 +0100

    Updating the code

--- projects/1-kepler_project/index.js
+++ projects/1-kepler_project/index.js
@@ -1,24 +1,25 @@
 function print(data) {
     console.log(data);
 }
-const parser = require('csv-parse');
+
+const csv = require('csv-parse');
 const fs = require('fs');
 
 const results = [];
 
-// fs.createReadStream('kepler_data.csv')
-//     .pipe(parser({
-//         comment: '#',
-//         colums: true,
-//     }))
-//     .on('data', (data) => {
-//         results.push(data);
-//     })
-//     .on('error', (err) => {
-//         console.log(err);
-//     })
-//     .on('end', () => {
-//         console.log(results);
-//         console.log('done');
-//     });
+fs.createReadStream('kepler_data.csv')
+    .pipe(csv.parse({
+        comment: '#',
+        colums: true,
+    }))
+    .on('data', (data) => {
+        results.push(data);
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(results);
+        console.log('done');
+    });
     

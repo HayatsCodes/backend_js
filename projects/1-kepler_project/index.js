00ae1c4ee882b5efbc62d1a1e33a7db023b224f3
HayatsCodes <hayatscodes@gmail.com>
2023-02-06 17:30:31 +0100

    Fixing a bug

--- projects/1-kepler_project/index.js
+++ projects/1-kepler_project/index.js
@@ -23,6 +23,7 @@ fs.createReadStream('kepler_data.csv')
         print(err);
     })
     .on('end', () => {
-        print(habitablePlanets.map(planet));
+        print(habitablePlanets.map(planet)) => {
+        };
         print(`${habitablePlanets.length} habitable planets found!`);
     });

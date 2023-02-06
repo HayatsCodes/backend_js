a6e6f4302472151afa3c66109b99aea0c36cdf32
HayatsCodes <hayatscodes@gmail.com>
2023-02-06 17:24:30 +0100

    Refactoring the code

--- projects/1-kepler_project/index.js
+++ projects/1-kepler_project/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 function isHabitablePlanet(planet) {
     return planet['koi_disposition'] === 'CONFIRMED' &&
-        planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet;
+        planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
 }
 
 const habitablePlanets = [];
@@ -23,6 +23,6 @@ fs.createReadStream('kepler_data.csv')
         console.log(err);
     })
     .on('end', () => {
-        console.log(habitablePlanets);
+        console.log(`{habitablePlanets}`);
         console.log('done');
     });

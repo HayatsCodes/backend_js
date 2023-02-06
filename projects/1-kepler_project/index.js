889dcb7daa67f0beca5ca7310edb3adacfee6a34
HayatsCodes <hayatscodes@gmail.com>
2023-02-06 16:56:10 +0100

    Adding a new feature

--- projects/1-kepler_project/index.js
+++ projects/1-kepler_project/index.js
@@ -8,18 +8,18 @@ const fs = require('fs');
 
 const results = [];
 
-// fs.createReadStream('kepler_data.csv')
-//     .pipe(csv.parse({
-//         comment: '#',
-//         colums: true,
-//     }))
-//     .on('data', (data) => {
-//         results.push(data);
-//     })
-//     .on('error', (err) => {
-//         console.log(err);
-//     })
-//     .on('end', () => {
-//         console.log(results);
-//         console.log('done');
-//     });
+fs.createReadStream('kepler_data.csv')
+    .pipe(csv.parse({
+        comment: '#',
+        colums: true,
+    }))
+    .on('data', (data) => {
+        results.push(data);
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(results);
+        console.log('done');
+    });

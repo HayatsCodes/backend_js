62348388b21eb649e97577d1113912ffa3a88465
HayatsCodes <hayatscodes@gmail.com>
2023-02-06 16:54:02 +0100

    Improving performance

--- projects/1-kepler_project/index.js
+++ projects/1-kepler_project/index.js
@@ -3,23 +3,23 @@ function print(data) {
 }
 
 const csv = require('csv-parse');
+print(csv.parse)
 const fs = require('fs');
 
 const results = [];
 
-fs.createReadStream('kepler_data.csv')
-    .pipe(csv.parse({
-        comment: '#',
-        colums: true,
-    }))
-    .on('data', (data) => {
-        results.push(data);
-    })
-    .on('error', (err) => {
-        console.log(err);
-    })
-    .on('end', () => {
-        console.log(results);
-        console.log('done');
-    });
-    
+// fs.createReadStream('kepler_data.csv')
+//     .pipe(csv.parse({
+//         comment: '#',
+//         colums: true,
+//     }))
+//     .on('data', (data) => {
+//         results.push(data);
+//     })
+//     .on('error', (err) => {
+//         console.log(err);
+//     })
+//     .on('end', () => {
+//         console.log(results);
+//         console.log('done');
+//     });

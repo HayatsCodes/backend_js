93925f6d600e5185bfc89e15272d6876d2d1f091
HayatsCodes <hayatscodes@gmail.com>
2023-02-06 15:54:21 +0100

    Refactoring the code

--- projects/1-kepler_project/index.js
+++ projects/1-kepler_project/index.js
@@ -3,11 +3,15 @@ const fs = require('fs');
 
 const results = [];
 
-fs.createReadStream('kepler_data.csv')
+fs.createReadStream('kepler_dataa.csv')
     .on('data', (data) => {
         results.push(data);
     })
+    .on('error', (err) => {
+        console.log(err);
+    })
     .on('end', () => {
-        console.log(results)
+        console.log(results);
+        console.log('done');
     })
     

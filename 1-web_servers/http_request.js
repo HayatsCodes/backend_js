1b4b13732b7305926e62748cac0b0abd7a08ebe9
HayatsCodes <hayatscodes@gmail.com>
2023-02-07 14:54:56 +0100

    Refactoring the code

--- 1-web_servers/http_request.js
+++ 1-web_servers/http_request.js
@@ -1,2 +1,16 @@
 // get:
-//  used to get 
+//  used to get information from a web server e.g get/messages, get/friends/15
+
+// post:
+// used to add new information to a web server e.g post/messages
+
+// put:
+// used to modify information in web server e.g put/messages/15
+
+// delete:
+// used to delete information in a web server e.g delete/friends/5
+
+// The 4 parts of a http request:
+// 1. Method: defines the operation that the browser wants to perform on the server e.g post/...
+// 2. Path: defines the resource that the browser wants to access in the server. e.g .../messages/
+// 3. Body: The data that is being sent/retrieved from the server e.g {text:"hello", photo:"smile.jpg"}

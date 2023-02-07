5e7da718ae0f8b8e68f60a915e65a26318dc7f60
HayatsCodes <hayatscodes@gmail.com>
2023-02-07 14:55:56 +0100

    Refactoring the code

--- 1-web_servers/http_request.js
+++ 1-web_servers/http_request.js
@@ -13,4 +13,5 @@
 // The 4 parts of a http request:
 // 1. Method: defines the operation that the browser wants to perform on the server e.g post/...
 // 2. Path: defines the resource that the browser wants to access in the server. e.g .../messages/
-// 3. Body: The data that is being sent/retrieved from the server e.g {text:"hello", photo:"smile.jpg"}
+// 3. Body: The data that is being sent to the server e.g {text:"hello", photo:"smile.jpg"}
+// 4. Headers: 

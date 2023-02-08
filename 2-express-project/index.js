c0cff48d18cd0a14e226168e3f4bae6752a7cef2
HayatsCodes <hayatscodes@gmail.com>
2023-02-08 23:53:29 +0100

    Improving performance

--- 2-express-project/index.js
+++ 2-express-project/index.js
@@ -1,3 +1,9 @@
 const express = require('express');
 
-const app = express()
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});

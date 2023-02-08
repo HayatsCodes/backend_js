4cdc6c26799992ad2b9ad6a304fa06886da2c866
HayatsCodes <hayatscodes@gmail.com>
2023-02-08 11:51:27 +0100

    Improving performance

--- 1-web_servers/index.js
+++ 1-web_servers/index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
-        'Content-Type': 'application',
+        'Content-Type': 'application/json',
     });
     res.end('Hello! respond finished!');
 });

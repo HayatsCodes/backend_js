898945b152a3ddf185bd77cab5f621c2e5e88a63
HayatsCodes <hayatscodes@gmail.com>
2023-02-08 16:45:18 +0100

    Updating the documentation

--- 1-web_servers/cross-origin.js
+++ 1-web_servers/cross-origin.js
@@ -3,3 +3,5 @@
 // 2. Host: It tells us which server we're going to be communicating with
 // 3. Port: where the application we want to access lives in the server
 
+// Same Origin Policy:
+// Restricts what 

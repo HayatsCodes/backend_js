ae4e1acf3d6c0f961ceaf31e73b1a1f417643a10
HayatsCodes <hayatscodes@gmail.com>
2023-02-08 16:44:18 +0100

    Adding a new feature

--- 1-web_servers/cross-origin.js
+++ 1-web_servers/cross-origin.js
@@ -2,3 +2,4 @@
 // 1. Protocol: What is being used to communicate with a web server
 // 2. Host: It tells us which server we're going to be communicating with
 // 3. Port: where the application we want to access lives in the server
+

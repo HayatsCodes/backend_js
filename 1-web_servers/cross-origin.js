060da68849e5fe584b7f4bd86bbf2cd147bf6110
HayatsCodes <hayatscodes@gmail.com>
2023-02-08 16:43:09 +0100

    Adding a new feature

--- 1-web_servers/cross-origin.js
+++ 1-web_servers/cross-origin.js
@@ -1,4 +1,4 @@
 // Origin is divided into three parts
 // 1. Protocol: What is being used to communicate with a web server
 // 2. Host: It tells us which server we're going to be communicating with
-// 3. Port: where the application we want to access lives
+// 3. Port: where the application we want to access lives in the server

aa94d4d562f051f9bc661db0b11afbeeea6ef942
HayatsCodes <hayatscodes@gmail.com>
2023-02-08 16:40:48 +0100

    Fixing a bug

--- 1-web_servers/cross-origin.js
+++ 1-web_servers/cross-origin.js
@@ -1,3 +1,4 @@
 // Origin is divided into three parts
 // 1. Protocol: What is being used to communicate with a web server
 // 2. Host: It tells us which server we're going to be communicating with
+// 3. Port: the specific resource  we want to z

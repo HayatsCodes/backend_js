a7e4c14d82e62801f2541f3d434b8f6666e53ead
HayatsCodes <hayatscodes@gmail.com>
2023-02-09 00:01:32 +0100

    Refactoring the code

--- 2-express-project/server.js
+++ 2-express-project/server.js
@@ -12,6 +12,10 @@ app.get('/messages', (req, res) => {
     res.send('<ul><li>Helloo Albert!</li></ul>')
 });
 
+app.post('/messages', (req, res) => {
+    console.log('Updating messages...');
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });

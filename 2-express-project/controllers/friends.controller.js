40ea22937ec390de3dc4441fe0fed4b4ba79730c
HayatsCodes <hayatscodes@gmail.com>
2023-02-09 23:36:32 +0100

    Improving performance

--- 2-express-project/controllers/friends.controller.js
+++ 2-express-project/controllers/friends.controller.js
@@ -1,17 +1,3 @@
-const friends = [
-    {
-        id: 0,
-        name: 'Nikola Tesla',
-    },
-    {
-        id: 1,
-        name: 'Sir Issac Newton',
-    },
-    {
-        id: 2,
-        name: 'Albert Einstein',
-    }
-]
 
 function postFriends(req, res) {
     if(!req.body.name) {

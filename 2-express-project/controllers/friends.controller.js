a1233df5635e369bb3e9f12ef8cbabbeed1946dc
HayatsCodes <hayatscodes@gmail.com>
2023-02-09 23:23:45 +0100

    Improving performance

--- 2-express-project/controllers/friends.controller.js
+++ 2-express-project/controllers/friends.controller.js
@@ -1,1 +1,13 @@
-function postFriends()
+function postFriends(req, res) {
+    if(!req.body.name) {
+        return res.status(400).json({
+            error: 'Missing friend name'
+        })
+    }
+    const newFriend = {
+        name: req.body.name,
+        id: friends.length
+    }
+    friends.push(newFriend);
+    res.json(newFriend);
+}

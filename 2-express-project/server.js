a12af0205640a8f03b77684990a0ac3ceea9b7b0
HayatsCodes <hayatscodes@gmail.com>
2023-02-09 22:32:58 +0100

    Adding a new feature

--- 2-express-project/server.js
+++ 2-express-project/server.js
@@ -30,10 +30,10 @@ app.use(express.json());
 
 app.post('/friends', (req, res) => {
     if(!req.body.name) {
-        res.status(400).json({
+        return res.status(400).json({
             error: 'Missing friend name'
         })
-    } 
+    }
     const newFriend = {
         name: req.body.name,
         id: friends.length

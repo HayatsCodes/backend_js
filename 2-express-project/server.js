f441854143b9109c550fb11458830db3ef2e2a37
HayatsCodes <hayatscodes@gmail.com>
2023-02-09 18:02:20 +0100

    Updating the code

--- 2-express-project/server.js
+++ 2-express-project/server.js
@@ -52,11 +52,11 @@ app.listen(PORT, () => {
 // they are special functions that runs between a request and a response coming out of an express api
 // it uses the app.use function and a call back function that accepts three parameters, namely:
 // req, res and next. Where req and res is used to process the request and the response coming fom an express api, while
-// next is used o move to the next middlewrae
-// example: app.use()
+// next is a function used to move to the next middlewrae
+// example: app.use((req, res, next) {})
 
 // Middleware endpoint:
 // This is the handler that matches the route the user is requesting
 // it uses the app.method function rather than the app.use function, 
 // where method are the common http methods like get, put e.t.c
-// i
+// 

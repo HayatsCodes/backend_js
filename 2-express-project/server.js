df6996a3fc27483444de7dbe1deb3dc2c53108d1
HayatsCodes <hayatscodes@gmail.com>
2023-02-10 15:03:01 +0100

    Updating the code

--- 2-express-project/server.js
+++ 2-express-project/server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
-
+const friendsRouter = express.Router();
 
 app.post('/friends', friendsController.postFriends);
 app.get('/friends', friendsController.getFriends);

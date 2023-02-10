17df8617a926d3d2f9270abf5a9d7a634d504e1a
HayatsCodes <hayatscodes@gmail.com>
2023-02-10 15:18:32 +0100

    Updating the documentation

--- 2-express-project/server.js
+++ 2-express-project/server.js
@@ -23,6 +23,8 @@ app.use(express.json());
 
 app.use('/friends', friendsRouter);
 
+
+
 app.get('/messages', messagesController.getMessages);
 app.post('/messages', messagesController.postMessages);
 

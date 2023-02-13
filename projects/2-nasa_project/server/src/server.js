a9614636509a54137f70f836f3f69d03911c04b1
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-13 15:52:13 +0100

    Updating the documentation

--- projects/2-nasa_project/server/src/server.js
+++ projects/2-nasa_project/server/src/server.js
@@ -1,7 +1,10 @@
-const PORT = process.env.PORT || 8000;
-
 const express = require('express');
 
 const app = express();
+app.listen();
+
+const PORT = process.env.PORT || 8000;
+
+
 
 console.log(PORT);

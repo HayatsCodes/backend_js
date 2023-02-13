672ac3c71120689c7f2aeac016418266185b6e0f
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-13 17:34:56 +0100

    Updating the documentation

--- projects/2-nasa_project/server/src/app.js
+++ projects/2-nasa_project/server/src/app.js
@@ -6,7 +6,7 @@ const { planetsRouter } = require('./routes/planets/planets.router');
 const app = express();
 
 app.use(cors({
-    origin:
+    origin: 'http://localhost:3000',
 }));
 app.use(express.json());
 app.use(planetsRouter);

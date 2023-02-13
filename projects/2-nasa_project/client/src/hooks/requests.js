e983772271204924fff739df8f90dd89df8755ab
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-13 17:40:36 +0100

    Improving performance

--- projects/2-nasa_project/client/src/hooks/requests.js
+++ projects/2-nasa_project/client/src/hooks/requests.js
@@ -1,24 +1,24 @@
 const API_URL = 'http://localhost:8000';
 
 // Load planets and return as JSON.
-async function httpGetPlanets() {
+function httpGetPlanets() {
     const response = await fetch(`${API_URL}/planets`);
     return await response.json();
 }
 
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  
+  // 
 }
 
 // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  
+  // 
 }
 
 // Delete launch with given ID.
 async function httpAbortLaunch(id) {
-  
+  // 
 }
 
 

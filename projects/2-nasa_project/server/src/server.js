392d2d83457e157ed536d5f34ee4053ae9c401ea
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-13 15:58:44 +0100

    Updating the documentation

--- projects/2-nasa_project/server/src/server.js
+++ projects/2-nasa_project/server/src/server.js
@@ -7,4 +7,8 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-console.log(PORT);
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
+
+// console.log(PORT);

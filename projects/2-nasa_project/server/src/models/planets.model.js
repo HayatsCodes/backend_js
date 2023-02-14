070a1ca6aa1e591911e628c914bdf715f99c458f
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-14 15:41:04 +0100

    Refactoring the code

--- projects/2-nasa_project/server/src/models/planets.model.js
+++ projects/2-nasa_project/server/src/models/planets.model.js
@@ -1,6 +1,7 @@
-const csv = require('csv-parse');
 const fs = require('fs');
 const path = require('path');
+const csv = require('csv-parse');
+
 
 
 
@@ -14,7 +15,7 @@ const habitablePlanets = [];
 
 function loadPlanetsData() {
     return new Promise((resolve, reject) => {
-        fs.createReadStream('kepler_data.csv')
+        fs.createReadStream(path.join(__dirname'kepler_data.csv'))
             .pipe(csv.parse({
                 comment: '#',
                 columns: true,

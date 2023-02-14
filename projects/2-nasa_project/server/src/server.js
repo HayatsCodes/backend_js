4022c3985650105ee5471bf6d2e1a04657288e4e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-14 15:27:44 +0100

    Improving performance

--- projects/2-nasa_project/server/src/server.js
+++ projects/2-nasa_project/server/src/server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+await loadPlanetsData();
+
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })

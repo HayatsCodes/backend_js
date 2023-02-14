03aeecfe706c60477ab5063b73391e4f5dbd14a8
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-14 15:04:15 +0100

    Refactoring the code

--- projects/2-nasa_project/server/src/models/planets.model.js
+++ projects/2-nasa_project/server/src/models/planets.model.js
@@ -1,3 +1,30 @@
-const planets = [];
+const { print } = require('../print')
+const csv = require('csv-parse');
+const fs = require('fs');
 
-module.exports = planets;
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' &&
+        planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
+}
+
+const habitablePlanets = [];
+
+fs.createReadStream('kepler_data.csv')
+    .pipe(csv.parse({
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
+    })
+    .on('error', (err) => {
+        print(err);
+    })
+    .on('end', () => {
+        print(habitablePlanets.map((planet) => {
+            return planet['kepler_name']
+        }));
+        print(`${habitablePlanets.length} habitable planets found!`);
+    });

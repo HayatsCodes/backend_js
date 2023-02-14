e6022f9f33ec337d37324b448771b0ee58b79095
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-14 13:59:17 +0100

    Refactoring the code

--- projects/2-nasa_project/server/src/routes/planets/planets.controller.js
+++ projects/2-nasa_project/server/src/routes/planets/planets.controller.js
@@ -1,6 +1,6 @@
 const planets = require('../../models/planets.model');
 
-console.log()
+console.log('planets: ', planets);
 
 function getAllPlanets(req, res) {
     return res.status(200).json(planets);

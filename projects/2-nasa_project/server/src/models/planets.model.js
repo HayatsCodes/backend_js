e4873aae062b6871642498eb6ffa38c218236a65
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-14 15:21:13 +0100

    Updating the code

--- projects/2-nasa_project/server/src/models/planets.model.js
+++ projects/2-nasa_project/server/src/models/planets.model.js
@@ -10,6 +10,23 @@ const habitablePlanets = [];
 
 function loadPlanetsData() {
     return new Promise((resolve, reject) => {
+        fs.createReadStream('kepler_data.csv')
+    .pipe(csv.parse({
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
+    })
+    .on('error', (err) => {
+        print(err);
+    })
+    .on('end', () => {
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        resolve();
+    });
 
     })
 }

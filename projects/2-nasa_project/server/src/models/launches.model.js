f5c95f66aac94a2327e3d6e9d5b0e1fae80736fe
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-17 21:15:29 +0100

    Refactoring the code

--- projects/2-nasa_project/server/src/models/launches.model.js
+++ projects/2-nasa_project/server/src/models/launches.model.js
@@ -2,5 +2,8 @@
 
   const launch = {
     flightNumber: 100,
-    mission
+    mission: 'Kepler Exploration X',
+    rocket: 'Explorer IS1',
+    launchDate: new Date('December 27, 2030');
+    destination: 'Kepler-442 b'
   }

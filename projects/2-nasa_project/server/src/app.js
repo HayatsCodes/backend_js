d4d15ba29211f4700240c347467ab0add1b52224
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-20 12:10:32 +0100

    Adding a new feature

--- projects/2-nasa_project/server/src/app.js
+++ projects/2-nasa_project/server/src/app.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
+app
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });

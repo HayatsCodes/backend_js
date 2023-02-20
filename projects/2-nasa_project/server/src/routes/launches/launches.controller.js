31dc46321c1dbca6804de0ed9a554d0fdc6127c3
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-20 13:59:34 +0100

    Refactoring the code

--- projects/2-nasa_project/server/src/routes/launches/launches.controller.js
+++ projects/2-nasa_project/server/src/routes/launches/launches.controller.js
@@ -1,4 +1,6 @@
 const { launches } = require('../../models/launches.model');
+console.log(Array.from(launches.values()));
+
 
 function getAllLaunches(req, res) {
     return res.status(200).json();

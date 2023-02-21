69bee71a6a8c414cb405e841101dbe06131ac580
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-21 22:42:18 +0100

    Refactoring the code

--- projects/2-nasa_project/client/src/hooks/requests.js
+++ projects/2-nasa_project/client/src/hooks/requests.js
@@ -17,7 +17,7 @@ async function httpGetLaunches() {
 
 // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
- retu await fetch(`${API_URL}/launches`, {
+ return await fetch(`${API_URL}/launches`, {
     method: "post",
     headers: {
       "Content-Type": "application/json",

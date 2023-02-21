8fe84b8f2b041bf28b057b08013a82c7c635e1d1
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-21 20:26:40 +0100

    Updating the documentation

--- projects/2-nasa_project/server/src/routes/launches/launches.controller.js
+++ projects/2-nasa_project/server/src/routes/launches/launches.controller.js
@@ -1,8 +1,12 @@
-const { getAlllaunches
+const { 
+    getAlllaunches,
+    addNewLaunch,
  } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(Array.from(getAlllaunches()));
 }
+
+
 
 module.exports = httpGetAllLaunches;

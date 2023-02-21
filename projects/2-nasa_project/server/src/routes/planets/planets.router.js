94f5dd9cd5bb86886364244b2bd3b65dfcf7507e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-21 19:25:02 +0100

    Adding a new feature

--- projects/2-nasa_project/server/src/routes/planets/planets.router.js
+++ projects/2-nasa_project/server/src/routes/planets/planets.router.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const { getAllPlanets } = require('../planets/planets.controller')
+const { httpGetAllPlanets } = require('../planets/planets.controller')
 
 const planetsRouter = express.Router();
 
-planetsRouter.get('/planets', getAllPlanets);
+planetsRouter.get('/planets', httpGetAllPlanets);
 
 module.exports = {
     planetsRouter

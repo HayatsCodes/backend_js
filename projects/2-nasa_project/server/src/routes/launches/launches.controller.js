62d8fcdd2e82d07d05d005d9a722b515390f1b28
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-22 16:59:58 +0100

    Improving performance

--- projects/2-nasa_project/server/src/routes/launches/launches.controller.js
+++ projects/2-nasa_project/server/src/routes/launches/launches.controller.js
@@ -20,19 +20,20 @@ function httpAddNewLaunch(req, res) {
         {name: 'mission', value:launch.mission},
         {name: 'rocket', value: launch.rocket},
         {name: 'launchDate', value: launch.launchDate},
-        {name: 'target', value: launch.target}
     ];
 
     for (let i = 0; i < launchProperties.length; i++) {
-        if (launchProperties[i].name === 'target' && !launchProperties[i].value) {
-           
-        }
-        else if (!launchProperties[i].value) {
+        if (!launchProperties[i].value) {
             return res.status(400).json({
                 error: `Missing required ${launchProperties[i].name} property`,
             });
         }
     }
+
+    const randomIndex = Math.floor(Math.random() * habitablePlanets.length);
+    const targetPlanet = habitablePlanets[randomIndex];
+
+    launch.target = targetPlanet;
 
     launch.launchDate = new Date(launch.launchDate);
     if (launch.launchDate.toString() === 'Invalid Date') {

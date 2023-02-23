dda44ff935d7a1ff5d3e7ea7d7dda249c66ec5b1
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-23 10:00:52 +0100

    Improving performance

--- projects/2-nasa_project/server/src/routes/launches/launches.router.js
+++ projects/2-nasa_project/server/src/routes/launches/launches.router.js
@@ -9,7 +9,7 @@ const launchesRouter = express.Router();
 
 launchesRouter.get('/', httpGetAllLaunches);
 launchesRouter.post('/', httpAddNewLaunch);
-
+launchesRouter.delete('/:id', abortLaunch);
 
 module.exports = {
     launchesRouter,

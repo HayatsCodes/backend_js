706bcf92e3faa461a3a108656c81a5ba6d31da11
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-24 18:06:42 +0100

    Improving performance

--- 3-node_performance/server.js
+++ 3-node_performance/server.js
@@ -36,7 +36,8 @@ if (cluster.isMaster) {
 
 
 
-
+// Load balancing is the techinque used to 
+// The node cluster module is used to do load balancing within a node server
 // When a node server is started, a master process is created.
 // With the cluster module, one can create multiple copies of the master process,
 // called workers by calling the fork() function provided in the cluster module.

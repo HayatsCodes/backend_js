6ee2530ea856854214671058ee3e18add543607b
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-24 20:57:13 +0100

    Adding a new feature

--- 3-node_performance/thread.js
+++ 3-node_performance/thread.js
@@ -2,7 +2,7 @@ const { isMainThread, Worker } = require('worker_threads');
 
 if (isMainThread) {
     console.log(`Main! Process ID: ${process.pid}`);
-    new Worker(__filename);
+    new Worker(__filename, );
     new Worker(__filename);
 } else {
     console.log(`Worker! Process ID: ${process.pid}`);

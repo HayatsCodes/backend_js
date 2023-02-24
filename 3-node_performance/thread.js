f9f3f471198e54be8a32e0e8ba50cac7f0620fd6
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-24 20:51:32 +0100

    Updating the documentation

--- 3-node_performance/thread.js
+++ 3-node_performance/thread.js
@@ -1,5 +1,8 @@
 const { isMainThread, Worker } = require('worker_threads');
 
 if (isMainThread) {
-    
+    new Worker(__filename);
+    new Worker(__filename);
+} else {
+    console.lopg('Worker');
 }

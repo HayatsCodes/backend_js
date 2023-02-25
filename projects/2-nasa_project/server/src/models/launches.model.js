08c79b09315c5ce318024ef85d21f518c2a15db4
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-25 21:57:31 +0100

    Fixing a bug

--- projects/2-nasa_project/server/src/models/launches.model.js
+++ projects/2-nasa_project/server/src/models/launches.model.js
@@ -1,21 +1,23 @@
-  const launchesDatabase = require('./launches.mongo');
-  
-  const launches = new Map();
+const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
 
-  let latestFlightNumber = 100;
+const launches = new Map();
 
-  const launch = {
-    flightNumber: 100,
-    mission: 'Kepler Exploration X',
-    rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    target: 'Kepler-442 b',
-    customers: ['ZTM', 'NASA'],
-    upcoming: true,
-    success: true,
-  };
+let latestFlightNumber = 100;
+
+const launch = {
+  flightNumber: 100,
+  mission: 'Kepler Exploration X',
+  rocket: 'Explorer IS1',
+  launchDate: new Date('December 27, 2030'),
+  target: 'Kepler-442 b',
+  customers: ['ZTM', 'NASA'],
+  upcoming: true,
+  success: true,
+};
 
 saveLaunch(launch);
+
 
 function existLaunchWithId(launchId) {
   return launches.has(launchId);
@@ -40,18 +42,18 @@ function addNewLaunch(launch) {
   latestFlightNumber++;
   launches.set(
     latestFlightNumber, Object.assign(launch, {
-    success: true,
-    upcoming: true,
-    customers: ['Zero to Mastery', 'NASA'],
-    flightNumber: latestFlightNumber,
-  }));
+      success: true,
+      upcoming: true,
+      customers: ['Zero to Mastery', 'NASA'],
+      flightNumber: latestFlightNumber,
+    }));
 }
 
 function abortLaunchById(launchId) {
-   const aborted = launches.get(launchId);
-   aborted.upcoming = false;
-   aborted.success = false;
-   return aborted;
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
 }
 
 module.exports = {

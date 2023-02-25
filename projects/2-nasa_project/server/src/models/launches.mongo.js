c25ae088351ea2e783cc0af132a02d916a131594
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-25 16:02:13 +0100

    Fixing a bug

--- projects/2-nasa_project/server/src/models/launches.mongo.js
+++ projects/2-nasa_project/server/src/models/launches.mongo.js
@@ -21,9 +21,7 @@ const launchesSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    customers: {
-        
-    },
+    customers: [ String ],
     upcoming: {
         type: Boolean,
         required: true,

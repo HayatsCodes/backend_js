05c4b7f565d8a30a0c8088428b19e6e803f2b88f
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-25 17:41:03 +0100

    Improving performance

--- projects/2-nasa_project/server/src/models/planets.model.js
+++ projects/2-nasa_project/server/src/models/planets.model.js
@@ -19,7 +19,9 @@ function loadPlanetsData() {
             }))
             .on('data', async (data) => {
                 if (isHabitablePlanet(data)) {
-                    await planets.create(data);
+                    await planets.create({
+                        keplerName: data.kepler_name,
+                    });
                 }
             })
             .on('error', (err) => {

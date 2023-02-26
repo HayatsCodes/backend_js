d22207cc07614008d6462d28ba09f4ee40ef4b41
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-26 22:09:43 +0100

    Updating the documentation

--- projects/2-nasa_project/server/src/routes/launches/launches.controller.js
+++ projects/2-nasa_project/server/src/routes/launches/launches.controller.js
@@ -61,6 +61,7 @@ async function httpAbortLaunch(req, res) {
     }
 
     const aborted = await abortLaunchById(launchId);
+    console.log(aborted)
 
     if (!aborted) {
         return res.status(404).json({

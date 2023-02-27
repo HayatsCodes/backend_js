f04ed049a5db7149a4962d76817b3e62d4ef0661
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-27 01:45:30 +0100

    Updating the code

--- projects/2-nasa_project/server/src/services/mongo.js
+++ projects/2-nasa_project/server/src/services/mongo.js
@@ -1,23 +1,27 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://nasa-api:4jNvIlTndydEdUSN@nasacluster.vkrtqnr.mongodb.net/nasa?retryWrites=true&w=majority';
+require('dotenv').config();
 
+// Update below to match your own MongoDB connection string.
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready!');
+  console.log('MongoDB connection ready!');
 });
 
-mongoose.connection.on('error', (err) =>
-console.error(err));
-
-mongoose.set('strictQuery', false);
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
 
 async function mongoConnect() {
-    await mongoose.connect(MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+  await mongoose.connect(MONGO_URL);
 }
+
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
+
 module.exports = {
-    mongoConnect,
+  mongoConnect,
+  mongoDisconnect,
 }

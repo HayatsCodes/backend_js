59788d55ca3bf023f880b86a770252d3999b9b1e
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-27 16:39:41 +0100

    Fixing a bug

--- projects/2-nasa_project/server/src/routes/api.js
+++ projects/2-nasa_project/server/src/routes/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
-const { planetsRouter } = require('./routes/planets/planets.router');
-const { launchesRouter } = require('./routes/launches/launches.router');
+const { planetsRouter } = require('./planets/planets.router');
+const { launchesRouter } = require('./launches/launches.router');
 
 const  api = express.Router();
 
-app.use('/v1/planets',planetsRouter);
-app.use('/v1/launches', launchesRouter);
+api.use('/v1/planets',planetsRouter);
+api.use('/v1/launches', launchesRouter);
+
+module.exports = api;

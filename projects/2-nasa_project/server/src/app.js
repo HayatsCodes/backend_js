da6b4a7aa954abc80db96ac0b435117130952575
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-27 09:05:10 +0100

    Improving performance

--- projects/2-nasa_project/server/src/app.js
+++ projects/2-nasa_project/server/src/app.js
@@ -18,7 +18,7 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/planets',planetsRouter);
+app.use('v1/planets',planetsRouter);
 app.use('/launches', launchesRouter);
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));

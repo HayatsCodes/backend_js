4275c8af46e40fdae06375e033a5103f81cfc47a
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-27 01:14:32 +0100

    Updating the documentation

--- projects/2-nasa_project/server/src/server.js
+++ projects/2-nasa_project/server/src/server.js
@@ -6,16 +6,10 @@ const { app } = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
-const MONGO_URL = 'mongodb+srv://nasa-api:4jNvIlTndydEdUSN@nasacluster.vkrtqnr.mongodb.net/nasa?retryWrites=true&w=majority';
 
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection ready!');
-});
 
-mongoose.connection.on('error', (err) =>
-console.error(err));
 
 mongoose.set('strictQuery', false);
 
--- projects/2-nasa_project/server/src/services/mongo.js
+++ projects/2-nasa_project/server/src/services/mongo.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose');
 
+const MONGO_URL = 'mongodb+srv://nasa-api:4jNvIlTndydEdUSN@nasacluster.vkrtqnr.mongodb.net/nasa?retryWrites=true&w=majority';
+
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) =>
+console.error(err));

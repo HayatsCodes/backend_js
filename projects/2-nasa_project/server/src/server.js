cee7a268c34ba5b68105154253d4fb435b5c4d82
Hayats <hayatudeenabdulrahman@gmail.com>
2023-02-27 17:47:39 +0100

    Updating the code

--- projects/2-nasa_project/server/src/server.js
+++ projects/2-nasa_project/server/src/server.js
@@ -15,6 +15,7 @@ const server = http.createServer(app);
 async function startServer() {
     await mongoConnect();
     await loadPlanetsData();
+    await 
 
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`);

6cc290f0dbf5a10e8fd2dff054675d0f64e2c0a3
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-06 00:33:17 +0100

    Adding a new feature

--- projects/2-nasa_project/server/src/models/launches.model.js
+++ projects/2-nasa_project/server/src/models/launches.model.js
@@ -126,6 +126,13 @@ async function saveLaunch(launch) {
 }
 
 async function scheduleNewLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error('No matching planet found')
+  }
   const newFlightNumber = await getLatestFlightNumber() + 1;
 
   const newLaunch = Object.assign(launch, {

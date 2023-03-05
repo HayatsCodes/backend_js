30ca2bcd46a5e0f5b1a1f94df9d8ad0328a06e84
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-06 00:24:37 +0100

    Refactoring the code

--- projects/2-nasa_project/server/src/models/launches.model.js
+++ projects/2-nasa_project/server/src/models/launches.model.js
@@ -24,6 +24,49 @@ saveLaunch(launch);
 
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
+async function populateLaunches () {
+  console.log('Downloading launch data...');
+  const response = await axios.post(SPACEX_API_URL, {
+    query: {},
+    options: {
+      pagination: false,
+        populate: [
+            {
+                path: 'rocket',
+                select: {
+                    name: 1
+                }
+            },
+            {
+              path: 'payloads',
+              select: {
+                'customers': 1
+              }
+            }
+        ]
+    }
+});
+
+  const launchDocs = response.data.docs
+
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc['payloads'];
+    const customers = payloads.flatMap((payload) => {
+      return payload['customers']
+    });
+    const launch = {
+      flightNumber: launchDoc['flight_number'],
+      mission: launchDoc['name'],
+      rocket: launchDoc['rocket']['name'],
+      launchDate: launchDoc['date_local'],
+      upcoming: launchDoc['upcoming'],
+      success: launchDoc['success'],
+      customers,
+    };
+    console.log(`${launch.flightNumber} ${launch.mission}`);
+  }
+}
+
 async function loadLaunchData() {
   const firstLaunch = await findLaunch({
     flightNumber: 1,

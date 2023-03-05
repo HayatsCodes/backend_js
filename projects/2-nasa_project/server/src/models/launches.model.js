3fcc8a1c8b65c5c40e5124a72af6e020875a0191
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-05 17:22:32 +0100

    Refactoring the code

--- projects/2-nasa_project/server/src/models/launches.model.js
+++ projects/2-nasa_project/server/src/models/launches.model.js
@@ -50,7 +50,19 @@ async function loadLaunchesData() {
   const launchDocs = response.data.docs
 
   for (const launchDoc of launchDocs) {
-    
+    const payloads = launchDoc['payloads'];
+    const customers = payloads.flatMap((payload) => {
+      return payload['customers']
+    });
+    const launch = {
+      flightNumber: launchDoc['flight_number'],
+      mission: launchDoc['name'],
+      rocket: launchDoc['rocket']['name'],
+      launchDate: launchDoc['date_local'],
+      upcoming: launchDoc['upcoming'],
+      success: launchDoc['success'],
+
+    }
   }
 }
 

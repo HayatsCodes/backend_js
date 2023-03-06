001b71618fbecc06965bf164172f4501bfae1220
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-06 01:10:03 +0100

    Refactoring the code

--- projects/2-nasa_project/server/src/services/query.js
+++ projects/2-nasa_project/server/src/services/query.js
@@ -1,4 +1,10 @@
 function getPagination(query) {
     const page = Math.abs(query.page);
     const limit = Math.abs(query.limit);
+    const skip = 
+
+    return {
+        skip,
+        limit,
+    }
 }

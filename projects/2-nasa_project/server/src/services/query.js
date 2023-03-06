bc620923021c4a8b5d961a57850bec9f540360ed
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-06 01:12:09 +0100

    Improving performance

--- projects/2-nasa_project/server/src/services/query.js
+++ projects/2-nasa_project/server/src/services/query.js
@@ -1,6 +1,6 @@
 function getPagination(query) {
-    const page = Math.abs(query.page);
-    const limit = Math.abs(query.limit);
+    const page = Math.abs(query.page) || 1;
+    const limit = Math.abs(query.limit) || 50;
     const skip = (page - 1) * limit;
 
     return {

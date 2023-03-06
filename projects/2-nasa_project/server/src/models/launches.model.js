9a9ca73e3e8a6dccaafecc7bc0238c380daa11d4
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-06 01:01:18 +0100

    Improving performance

--- projects/2-nasa_project/server/src/models/launches.model.js
+++ projects/2-nasa_project/server/src/models/launches.model.js
@@ -118,8 +118,7 @@ async function getLatestFlightNumber() {
 
 async function getAllLaunches() {
   return await launchesDatabase.find({}, {
-    '_id': 0, '__v': 0,
-  });
+    '_id': 0, '__v': 0,});
 }
 
 async function saveLaunch(launch) {

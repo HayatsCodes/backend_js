76970e0a6f67f9d176b4cc27d85d203fa0755f79
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-06 01:18:49 +0100

    Updating the documentation

--- projects/2-nasa_project/server/src/models/launches.model.js
+++ projects/2-nasa_project/server/src/models/launches.model.js
@@ -116,11 +116,11 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
+async function getAllLaunches(skip, limit) {
   return await launchesDatabase
   .find({}, {'_id': 0, '__v': 0,})
-  .skip()
-  .limit()
+  .skip(skip)
+  .limit(limit);
 }
 
 async function saveLaunch(launch) {

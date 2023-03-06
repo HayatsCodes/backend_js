efc1e0fb6dc17061cdbb7bd2439cccb7e181ac6b
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-06 01:14:27 +0100

    Updating the code

--- projects/2-nasa_project/server/src/services/query.js
+++ projects/2-nasa_project/server/src/services/query.js
@@ -1,12 +1,17 @@
+const DEFAULT_PAGE_NUMBER = 1;
 const DEFAULT_PAGE_LIMIT = 0;
 
 function getPagination(query) {
-    const page = Math.abs(query.page) || 1;
-    const limit = Math.abs(query.limit) || 50;
+    const page = Math.abs(query.page) || DEFAULT_PAGE_NUMBER;
+    const limit = Math.abs(query.limit) || DEFAULT_PAGE_LIMIT;
     const skip = (page - 1) * limit;
 
     return {
         skip,
         limit,
     }
+}
+
+module.exports = {
+    
 }

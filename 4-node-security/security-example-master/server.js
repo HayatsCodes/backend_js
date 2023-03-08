20264cd3f0263ae46ba969a472ada166cb5ab750
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-08 15:51:43 +0100

    Updating the documentation

--- 4-node-security/security-example-master/server.js
+++ 4-node-security/security-example-master/server.js
@@ -1,49 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
-// const express = require('express');
+const express = require('express');
 // const helmet = require('helmet');
 // const passport = require('passport');
 // const { Strategy } = require('passport-google-oauth20');
 // const cookieSession = require('cookie-session');
 // const { verify } = require('crypto');
 
-require('dotenv').config();
 
 const PORT = 3000;
-
-const config = {
-  CLIENT_ID: process.env.CLIENT_ID,
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
-  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
-  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
-};
-
-const AUTH_OPTIONS = {
-  callbackURL: '/auth/google/callback',
-  clientID: config.CLIENT_ID,
-  clientSecret: config.CLIENT_SECRET,
-};
-
-function verifyCallback(accessToken, refreshToken, profile, done) {
-  console.log('Google profile', profile);
-  done(null, profile);
-}
-
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
-
-// Save the session to the cookie
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-// Read the session from the cookie
-passport.deserializeUser((id, done) => {
-  // User.findById(id).then(user => {
-  //   done(null, user);
-  // });
-  done(null, id);
-});
 
 const app = express();
 
@@ -58,6 +24,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+app.listen(PORT, () => {
+  console.log(`listening on port 3000`);
 })

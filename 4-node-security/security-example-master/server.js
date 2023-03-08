36c6f44183c1fff1cc4bafce9b244680f1da8300
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-08 15:49:36 +0100

    Improving performance

--- 4-node-security/security-example-master/server.js
+++ 4-node-security/security-example-master/server.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const https = require('https');
-const express = require('express');
-const helmet = require('helmet');
-const passport = require('passport');
-const { Strategy } = require('passport-google-oauth20');
-const cookieSession = require('cookie-session');
-const { verify } = require('crypto');
+// const express = require('express');
+// const helmet = require('helmet');
+// const passport = require('passport');
+// const { Strategy } = require('passport-google-oauth20');
+// const cookieSession = require('cookie-session');
+// const { verify } = require('crypto');
 
 require('dotenv').config();
 
@@ -101,9 +101,4 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-https.createServer({
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-}, app).listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+app.listen()

f046bac01fe6f841372c91daf0cb97c1b21584d0
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-10 00:33:04 +0100

    Fixing a bug

--- 4-node-security/security-example-master/server.js
+++ 4-node-security/security-example-master/server.js
@@ -58,7 +58,7 @@ app.use(passport.initialize());
 app.use(passport.session())
 
 function checkLoggedIn(req, res, next) {
-  const isLoggedIn = true;
+  const isLoggedIn = req;
   if (!isLoggedIn) {
     return res.status(401).json({
       error: 'You must login!',

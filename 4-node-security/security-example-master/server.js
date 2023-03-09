62d63f603b848c3e25c7e4e764f59042e2081f8b
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-09 22:33:34 +0100

    Adding a new feature

--- 4-node-security/security-example-master/server.js
+++ 4-node-security/security-example-master/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const helmet = require('helmet');
 const passport = require('passport');
 const { Strategy } = require('passport-google-oauth20');
-// const cookieSession = require('cookie-session');
+const cookieSession = require('cookie-session');
 // const { verify } = require('crypto');
 
 require('dotenv').config();

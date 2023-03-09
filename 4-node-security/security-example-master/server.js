8cff688d91b49d6cd7286f4f13ce720130103c70
Hayats <hayatudeenabdulrahman@gmail.com>
2023-03-09 12:30:22 +0100

    Fixing a bug

--- 4-node-security/security-example-master/server.js
+++ 4-node-security/security-example-master/server.js
@@ -3,11 +3,13 @@ const path = require('path');
 const https = require('https');
 const express = require('express');
 const helmet = require('helmet');
-
-// const passport = require('passport');
-// const { Strategy } = require('passport-google-oauth20');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
 // const cookieSession = require('cookie-session');
 // const { verify } = require('crypto');
+
+require('dotenv').config();
+
 
 
 const PORT = 3000;
@@ -20,6 +22,7 @@ const config = {
 const app = express();
 
 app.use(helmet());
+app.use
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true;

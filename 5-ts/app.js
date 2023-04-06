8d9cf6e94d88b4f1db1f18f8309963bf5eb93195
Hayats <hayatudeenabdulrahman@gmail.com>
2023-04-06 09:18:58 +0100

    Adding a new feature

--- 5-ts/app.js
+++ 5-ts/app.js
@@ -1,10 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var http_1 = require("http");
-http_1.default
-    .createServer(function (req, res) {
+http_1.default.createServer(function (req, res) {
     res.end('Hello world');
 })
     .listen(8080, function () {
-    console.log('Server started at ');
+    console.log('Server started at port 8080');
 });
--- 5-ts/app.ts
+++ 5-ts/app.ts
@@ -1,9 +1,8 @@
 import http from 'http'
 
-http
-    .createServer((req, res) => {
+http.createServer((req, res) => {
         res.end('Hello world');
     })
     .listen(8080, () => {
-        console.log('Server started at ')
-    })
+        console.log('Server started at port 8080');
+    });

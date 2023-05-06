017ad55e919d36523166d3e5275ce7418cf2b882
Hayats <hayatudeenabdulrahman@gmail.com>
2023-05-06 17:23:01 +0100

    create graphql server

--- 5-graphql/server.js
+++ 5-graphql/server.js
@@ -1,26 +1,9 @@
-const express = require('express');
-const app = express();
+const { ApolloServer } = require('apollo-server');
 
-const { buildSchema } = require('graphql');
-const { createHandler }= require('graphql-http');
+const server = new ApolloServer({ typeDefs, resolvers});
 
-const schema = buildSchema(`
-    type Query {
-        description: String
-        price: Float
-    }
-`);
-
-const root = {
-    description: 'Red Shoe',
-    price: 42.12
-}
-
-app.use(express.json());
-
-app.use('/graphql', createHandler({ schema, rootValue: root }));
-
-
-app.listen(4000, () => {
-    console.log('Server started on port 4000');
+server.listen().then(({url}) => {
+    console.log(`Your API is running at ${url}:)`)
 });
+
+
